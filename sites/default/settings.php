bbdcb2d96a187a4228f7023ac85f12102ff30273
Epicodus <student@epicodus.com>
2015-05-19 08:29:47 -0700

    Add settings.php from post installation.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -212,7 +212,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'hookpractice',
+      'username' => 'hookpractice',
+      'password' => 'hookpractice',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -244,7 +258,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'XvU50_1g-CrPcBk-gnc9Hgs0sn3rSfgJddiNXYGUh04';
 
 /**
  * Base URL (optional).
